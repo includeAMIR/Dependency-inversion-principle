4794a738ab560af69e82a6317eb7243325f8e1d5
Amir <amir.benmallem@gmail.com>
2020-05-13 18:44:13 +0200

    modif pour respecter DIP

---
+++ gl35/src/main/java/Amir/gl35/Affichage.java
@@ -0,0 +1,5 @@
+package Amir.gl35;
+
+public interface Affichage {
+	public void afficher();
+}
---
+++ gl35/src/main/java/Amir/gl35/Affiche.java
@@ -0,0 +1,10 @@
+package Amir.gl35;
+
+public class Affiche implements Affichage {
+
+	public void afficher() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
